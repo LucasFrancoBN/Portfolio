b852cb51dfb542a42ca62a912c85f4642f9664a5
Lucas <lucasfbnavarro@gmail.com>
2022-08-26 08:17:25 -0300

    resolucao bug menu mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -3,18 +3,15 @@ export default function initMenuMobile() {}
 
 const menuButton = document.querySelector("[data-menu='button']");
 const menuList = document.querySelector("[data-menu='list']");
-const events = ["click", "touchstart"];
+const userEvent = "click";
 
 function handleClick() {
   menuButton.classList.add("ativo");
   menuList.classList.add("ativo");
   console.log(menuButton, menuList);
-  outsideClick(menuList, events, () => {
+  outsideClick(menuList, userEvent, () => {
     menuButton.classList.remove("ativo");
     menuList.classList.remove("ativo");
   });
 }
-
-events.forEach((userEvent) =>
-  menuButton.addEventListener(userEvent, handleClick)
-);
+menuButton.addEventListener(userEvent, handleClick);
